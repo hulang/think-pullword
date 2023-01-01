dafe08abff409cacf93c0d844dfcb84ce7054c3a
快乐浪子哥 <hulangfy@163.com>
2023-01-01 14:24:52 +0800

    update

---
+++ src/Service/Pull.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PullWord\Service;
+
+use PullWord\Service;
+
+/**
+ * 分词服务
+ * 
+ */
+class Pull extends Service
+{
+    protected $uri = 'http://api.pullword.com/get.php';
+}
