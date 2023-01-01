2f0e2614c7a6d5d598d40fe8f66dae7f514e6161
快乐浪子哥 <hulangfy@163.com>
2023-01-01 14:23:05 +0800

    update

---
+++ src/Exception/HttpException.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PullWord\Exception;
+
+/**
+ * Class HttpException.
+ */
+class HttpException extends Exception
+{
+}
---
+++ src/Exception/InvalidArgumentException.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PullWord\Exception;
+
+/**
+ * Class InvalidArgumentException.
+ */
+class InvalidArgumentException extends Exception
+{
+}
